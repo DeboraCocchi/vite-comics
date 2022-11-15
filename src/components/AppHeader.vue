357d121c7fed34a3dc3bbe36e89e5b937510f176
Debora Cocchi <108935821+DeboraCocchi@users.noreply.github.com>
2022-11-15 16:04:43 +0100

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name:'AppHeader'
+}
+</script>
+
+<template>
+  <header class="d-flex">
+    <div class="container">
+      <div class="logo">
+      <img src="/dc-logo.png" alt="DC Logo">
+    </div>
+    </div>
+    
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+  @use '../style/partials/variables' as *;
+  @use '../style/partials/mixin' as *;
+
+  header{
+    background-color: white;
+    height:85px;
+    margin-top:30px;
+    .logo{
+      padding:10px;
+    }
+  }
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name:'AppMain'
+}
+</script>
+
+<template>
+  
+</template>
+
+
+<style lang="scss" scoped>
+  @use '../style/partials/variables' as *;
+  @use '../style/partials/mixin' as *;
+</style>
